0488c445c1b39330b9617bc8edb4867a34b87b60
mayaffia <maifraimova@edu.hse.ru>
2024-10-06 18:41:16 +0300

    c

--- HW1/hard.ts
+++ HW1/hard.ts
@@ -1,3 +1,57 @@
-export type Camelize<ObjectType> = unknown;
+//export type Camelize<ObjectType> = unknown;
 
-export type DeepPick<T, Paths> = unknown;
+type CamelToSnakeCase<S extends string> = S extends `${infer Head}_${infer Tail}`
+    ? `${Head}${Capitalize<CamelToSnakeCase<Tail>>}`
+    : S;
+
+export type Camelize<T> = T extends object
+    ? {
+        [K in keyof T as CamelToSnakeCase<K & string>]: Camelize<T[K]>
+    }
+    : T;
+
+
+
+type DeepPickHelper<T, K extends keyof T> = K extends string
+    ? T[K] extends object
+        ? { [P in K]: DeepPick<T[K], Extract<keyof T[K], string>> }
+        : { [P in K]: T[K] }
+    : never;
+
+export type DeepPick<T, Paths extends string> =
+    Paths extends `${infer Key}.${infer Rest}`
+    ? Key extends keyof T
+    ? DeepPickHelper<T, Key> & DeepPick<T[Key], Rest>
+    : never
+: Paths extends keyof T
+    ? { [K in Paths]: T[K] }
+    : never;
+
+
+// Примеры использования
+
+/* Camelize */
+interface SnakeCase {
+    person_name: string;
+    person_age: number;
+    person_details: {
+        first_name: string;
+        last_name: string;
+    };
+}
+
+type CamelCaseExample = Camelize<SnakeCase>;
+
+/* DeepPick */
+interface Example {
+    person: {
+        name: string;
+        details: {
+            age: number;
+            address: string;
+        };
+    };
+    isActive: boolean;
+}
+
+type PersonDetails = DeepPick<Example, 'user.details.address' | 'isActive'>;
--- HW1/medium.ts
+++ HW1/medium.ts
@@ -1,7 +1,83 @@
-export type DeepPartial<T> = unknown;
 
-export type MyCapitalize<T> = unknown;
+export type DeepPartial<T> = {
+    [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+};
 
-export type DeepMutable<T> = unknown;
+export type MyCapitalize<T extends string> = T extends `${infer F}${infer R}`
+    ? `${Uppercase<F>}${R}` : T;
 
-export type ParseURLParams<StringElem> = unknown;
+export type DeepMutable<T> = {
+    -readonly [K in keyof T]: T[K] extends object ? DeepMutable<T[K]> : T[K];
+};
+
+export type ParseURLParams<S extends string> =
+    S extends `${infer _Prefix}:${infer Param}/${infer Rest}`
+    ? Param | ParseURLParams<Rest>
+    : S extends `${infer _Prefix}:${infer Param}`
+    ? Param
+    : never;
+
+
+
+// Примеры использования
+
+/* DeepPartial */
+interface Person {
+    name: string;
+    age: number;
+    address: {
+        street: string;
+        house: number;
+    };
+}
+
+const partialPerson: DeepPartial<Person> = {
+    name: "Maya",
+    address: {
+        house: 10
+    }
+};
+
+
+/* MyCapitalize */
+type CapitalizedHello = MyCapitalize<'something'>; // 'Hello'
+
+const smth: CapitalizedHello = 'Something';
+// const s: CapitalizedHello = 'something'; а так уже нельзя
+
+console.log(smth)
+
+
+/* DeepMutable */
+interface ReadonlyPerson {
+    readonly age: number;
+    readonly name: string;
+    readonly address: {
+        readonly street: string;
+        readonly house: number;
+    };
+}
+
+type MutablePerson = DeepMutable<ReadonlyPerson>;
+
+const mutablePerson: MutablePerson = {
+    age: 20,
+    name: "Maya",
+    address: {
+        street: "Some streer",
+        house: 10
+    }
+};
+
+mutablePerson.name = "Alice";
+mutablePerson.address.house = 20;
+
+
+
+/* ParseURLParams */
+
+type Params = ParseURLParams<'posts/:id/:user'>; // 'id' | 'user'
+
+const param1: Params = 'id';
+const param2: Params = 'user';
+// const param3: Params = 'kk'; а так уже сделать нельзя
